c0ef1db28f1abc6f452d93414959e8741e663bad
Ilari <ilari.k.paananen@student.jyu.fi>
2016-06-16 21:25:53 +0300

    Calls yasm and gcc

--- src/main.cpp
+++ src/main.cpp
@@ -6,88 +6,120 @@
 #include "alloc.h"
 #include "error_context.h"
 
-int compile(const char *, const char *);
+#include <cstdlib>
+
+enum OutputMode
+{
+    OutputMode_EXE,
+    OutputMode_ASM,
+    OutputMode_OBJ,
+};
+
+int compile(const char *source_file, const char *output_file);
+
+int invoke(const char *command)
+{
+    return system(command);
+}
+
+int invoke(const char *fmt, const char *param)
+{
+    char command[256];
+    snprintf(command, sizeof(command), fmt, param);
+    return system(command);
+}
+
+void print_help()
+{
+    fprintf(stdout,
+            "Usage: mug [-s|-c] [-o <output-file>] <source-file>\n\n"
+            "By default, mug runs three phases:\n"
+            "(1) <source-file> ==> mug ==> out.s\n"
+            "(2) out.s ==> nasm ==> out.o\n"
+            "(3) out.o ==> gcc ==> out.exe\n\n"
+            "If -s is given, mug stops after phase (1).\n"
+            "If -c is given, mug stops after phase (2).\n"
+            "If both -s and -c are given, only the last one is effective.\n"
+            "If -o is given, the final output of mug is <output-file>.\n\n"
+            "Example usage: mug -c -o banana.o banana.mug\n");
+}
 
 int main(int argc, const char **argv)
 {
-    /*const char *args[] = {
-        "", "-p", "tests/code_gen_tests.mug"
-    };
-    argc = sizeof(args) / sizeof(*args);
-    argv = args;*/
-
     if (argc < 2)
-        run_tests();
-    else
     {
-        const char *s = nullptr;
-        const char *o = nullptr;
-        bool print_only = false;
-        char buf[256];
-
-        for (int i = 1; i < argc; i++)
-        {
-            const char *arg = argv[i];
-            if (arg[0] == '-')
-            {
-                if (arg[1] == 'o')
-                {
-                    if (arg[2])
-                        o = arg + 2;
-                    else if (i + 1 < argc)
-                        o = argv[++i];
-                    else
-                    {
-                        fprintf(stderr, "error: -o flag requires output file!\n");
-                        return 1;
-                    }
-                }
-                else if (arg[1] == 'p')
-                {
-                    print_only = true;
-                }
-                else
-                {
-                    fprintf(stderr, "warning: unrecognized parameter %s\n", arg);
-                }
-            }
-            else
-            {
-                if (s != nullptr)
-                {
-                    fprintf(stderr, "error: mug can handle only one source file at a time!\n");
-                    return 1;
-                }
-
-                s = arg;
-            }
-        }
-
-        if (s == nullptr)
-        {
-            fprintf(stderr, "error: source file not given!\n");
-            return 1;
-        }
-
-        if (o == nullptr && !print_only)
-        {
-            char *dest = buf;
-            for (unsigned i = 0; i < sizeof(buf) - 3; i++)
-            {
-                if (s[i] == 0 || s[i] == '.') break;
-                *dest++ = s[i];
-            }
-            *dest++ = '.';
-            *dest++ = 's';
-            *dest++ = 0;
-
-            o = buf;
-        }
-
-        return compile(s, print_only ? nullptr : o);
+        print_help();
+        return 0;
     }
 
-    return 0;
+    const char *source = nullptr;
+    const char *output = nullptr;
+    OutputMode mode = OutputMode_EXE;
+    char buf[1024];
+
+    for (int i = 1; i < argc; i++)
+    {
+        const char *arg = argv[i];
+        if (arg[0] == '-')
+        {
+            switch (arg[1])
+            {
+                case 's':
+                    mode = OutputMode_ASM;
+                    break;
+                case 'c':
+                    mode = OutputMode_OBJ;
+                    break;
+                case 'o':
+                {
+                    if (arg[2])
+                        output = arg + 2;
+                    else if (i + 1 < argc)
+                        output = argv[++i];
+                    else
+                    {
+                        fprintf(stderr, "error: -o requires output file\n");
+                        return 1;
+                    }
+                    break;
+                }
+                default:
+                    fprintf(stderr, "warning: unrecognized parameter %s\n", arg);
+                    break;
+            }
+        }
+        else
+        {
+            if (source != nullptr)
+            {
+                fprintf(stderr, "error: mug can handle only one source file at a time!\n");
+                return 1;
+            }
+
+            source = arg;
+        }
+    }
+
+    if (source == nullptr)
+    {
+        fprintf(stderr, "error: source file not given!\n");
+        return 1;
+    }
+
+
+    if (mode == OutputMode_ASM)
+        return compile(source, output ? output : "out.s");
+
+    if (compile(source, "out.s") != 0)
+        return 1;
+
+    if (mode == OutputMode_OBJ)
+        return invoke("yasm -f win64 -o %s out.s", output ? output : "out.o");
+
+    if (invoke("yasm -f win64 -o out.o out.s") != 0)
+        return 1;
+
+    return invoke("gcc -o %s out.o", output ? output : "out.exe");
 }
 
 //
@@ -133,23 +165,16 @@ int compile(const char *source_file, const char *output_file)
 
     IR ir = gen_ir(ast, a);
 
-    if (output_file)
+    FILE *f = fopen(output_file, "w");
+    if (f == nullptr)
     {
-        FILE *f = fopen(output_file, "w");
-        if (f == nullptr)
-        {
-            fprintf(stderr, "error: couldn't open file '%s' for writing\n", output_file);
-            return 1;
-        }
-
-        gen_code(ir, f);
-
-        fclose(f);
+        fprintf(stderr, "error: couldn't open file '%s' for writing\n", output_file);
+        return 1;
     }
-    else
-    {
-        gen_code(ir, stdout);
-    }
+
+    gen_code(ir, f);
+
+    fclose(f);
 
     return 0;
 }
